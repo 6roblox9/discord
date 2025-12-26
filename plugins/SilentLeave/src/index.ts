ea7234245d00ba5967237c6e55157ac273957eab
6roblox9 <croissantai69@gmail.com>
2025-12-26 04:48:50 +0300

    Update index.ts

--- plugins/SilentLeave/src/index.ts
+++ plugins/SilentLeave/src/index.ts
@@ -1,11 +1,10 @@
-import { findByProps } from '@vendetta/metro';
+import { metro } from '@vendetta';
 import { instead } from '@vendetta/patcher';
 import { storage } from '@vendetta/plugin';
 import Settings from "./Settings";
 
-const HTTP = findByProps("del", "put", "post");
-const REST = findByProps("delete", "patch", "post");
-const Agent = findByProps("delete", "put");
+const ChannelActions = metro.findByProps("closePrivateChannel", "leaveGroupDM");
+const Dispatcher = metro.findByProps("dispatch", "subscribe");
 
 let patches = [];
 
@@ -13,33 +12,32 @@ export default {
     onLoad: () => {
         storage.logs ??= [];
         const timestamp = new Date().toLocaleTimeString();
-        storage.logs.unshift(`[${timestamp}] Plugin Started - Monitoring...`);
+        storage.logs.unshift(`[${timestamp}] Advanced Monitor Started`);
 
-        const handleRequest = (args, orig, method) => {
-            const request = args[0];
-            let url = typeof request === "string" ? request : request?.url;
-
-            if (typeof url === "string" && url.includes("/channels/")) {
-                storage.logs.unshift(`[${timestamp}] ${method} DETECTED: ${url}`);
-                if (storage.logs.length > 30) storage.logs.pop();
-
-                if (!url.includes("silent=true")) {
-                    const separator = url.includes("?") ? "&" : "?";
-                    const newUrl = `${url}${separator}silent=true`;
-                    if (typeof request === "string") {
-                        args[0] = newUrl;
-                    } else if (request?.url) {
-                        request.url = newUrl;
-                    }
-                    storage.logs.unshift(`[${timestamp}] MODIFIED: Added silent=true`);
+        if (ChannelActions) {
+            patches.push(instead("closePrivateChannel", ChannelActions, (args, orig) => {
+                const time = new Date().toLocaleTimeString();
+                storage.logs.unshift(`[${time}] Intercepted: closePrivateChannel`);
+                
+                if (args[1] !== true) {
+                    args[1] = true; 
+                    storage.logs.unshift(`[${time}] Success: Set silent=true (Arg)`);
                 }
-            }
-            return orig(...args);
-        };
+                return orig(...args);
+            }));
+        }
 
-        if (HTTP?.del) patches.push(instead("del", HTTP, (args, orig) => handleRequest(args, orig, "HTTP")));
-        if (REST?.delete) patches.push(instead("delete", REST, (args, orig) => handleRequest(args, orig, "REST")));
-        if (Agent?.delete) patches.push(instead("delete", Agent, (args, orig) => handleRequest(args, orig, "Agent")));
+        if (Dispatcher) {
+            patches.push(instead("dispatch", Dispatcher, (args, orig) => {
+                const action = args[0];
+                if (action.type === "CHANNEL_DELETE" || action.type === "LEAVE_GROUP_DM") {
+                    const time = new Date().toLocaleTimeString();
+                    storage.logs.unshift(`[${time}] Dispatcher: ${action.type}`);
+                    action.silent = true;
+                }
+                return orig(...args);
+            }));
+        }
     },
     onUnload: () => {
         patches.forEach(unpatch => unpatch());
