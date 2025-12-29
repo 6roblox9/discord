c136d7c8f6eecb3f21938d8412140ec20bc95e01
6roblox9 <croissantai69@gmail.com>
2025-12-29 22:41:30 +0000

    Update logger.ts

--- plugins/quest/logger.ts
+++ plugins/quest/logger.ts
@@ -1,32 +1,33 @@
 export type LogEntry = {
-  id: string;
-  color: "G" | "Y" | "C" | "X" | "R";
-  text: string;
-  time: string;
+    id: string;
+    color: "G" | "Y" | "C" | "X";
+    text: string;
+    time: string;
 };
 
 export const logs: LogEntry[] = [];
 let listeners: (() => void)[] = [];
 
 export function subscribe(callback: () => void) {
-  listeners.push(callback);
-  return () => {
-    listeners = listeners.filter(l => l !== callback);
-  };
+    listeners.push(callback);
+    return () => {
+        listeners = listeners.filter(l => l !== callback);
+    };
 }
 
 export function log(color: LogEntry["color"], text: string) {
-  const time = new Date().toLocaleTimeString();
-  logs.push({
-    id: Math.random().toString(36).substring(7),
-    color,
-    text,
-    time
-  });
-  listeners.forEach(l => l());
+    const time = new Date().toLocaleTimeString();
+    logs.push({
+        id: Math.random().toString(36).substring(7),
+        color,
+        text,
+        time
+    });
+    listeners.forEach(l => l());
 }
 
 export function clearLogs() {
-  logs.length = 0;
-  listeners.forEach(l => l());
+    logs.length = 0;
+    listeners.forEach(l => l());
 }
+
