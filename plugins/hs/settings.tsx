33273590b8d4e7125a07431543a97c205d58d44e
6roblox9 <croissantai69@gmail.com>
2026-01-18 21:17:36 +0300

    Refactor settings component to use useProxy

--- plugins/hs/settings.tsx
+++ plugins/hs/settings.tsx
@@ -1,41 +1,34 @@
 import { React, ReactNative as RN } from "@vendetta/metro/common";
 import { storage } from "@vendetta/plugin";
+import { useProxy } from "@vendetta/storage";
 import { FormSwitchRow } from "@vendetta/ui/components";
 
+import { findByProps } from "@vendetta/metro";
+const { ScrollView } = findByProps("ScrollView");
+
 export default () => {
-    const [fakeMute, setFakeMute] = React.useState(storage.fakeMute ?? false);
-    const [fakeDeaf, setFakeDeaf] = React.useState(storage.fakeDeaf ?? false);
-    const [fakeVideo, setFakeVideo] = React.useState(storage.fakeVideo ?? false);
+    useProxy(storage);
 
     return (
-        <RN.ScrollView>
+        <ScrollView>
             <FormSwitchRow
                 label="Fake Mute"
-                subLabel="Show others you are muted, but you can still talk (locally unmuted)."
-                value={fakeMute}
-                onValueChange={(value) => {
-                    setFakeMute(value);
-                    storage.fakeMute = value;
-                }}
+                subLabel="Show others you are muted, but you can still talk."
+                value={storage.fakeMute ?? false}
+                onValueChange={(v) => storage.fakeMute = v}
             />
             <FormSwitchRow
                 label="Fake Deafened"
-                subLabel="Show others you are deafened, but you can still hear them."
-                value={fakeDeaf}
-                onValueChange={(value) => {
-                    setFakeDeaf(value);
-                    storage.fakeDeaf = value;
-                }}
+                subLabel="Show others you are deafened, but you can still hear."
+                value={storage.fakeDeaf ?? false}
+                onValueChange={(v) => storage.fakeDeaf = v}
             />
             <FormSwitchRow
                 label="Fake Video Off"
                 subLabel="Show others your video is off, but you can still stream."
-                value={fakeVideo}
-                onValueChange={(value) => {
-                    setFakeVideo(value);
-                    storage.fakeVideo = value;
-                }}
+                value={storage.fakeVideo ?? false}
+                onValueChange={(v) => storage.fakeVideo = v}
             />
-        </RN.ScrollView>
+        </ScrollView>
     );
 };
