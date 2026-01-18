260b04971452d8ca655c245b39b921fd30d8185a
6roblox9 <croissantai69@gmail.com>
2026-01-18 21:06:35 +0300

    Refactor voice state handling in HS plugin
    Refactor plugin to use patching for voice state updates instead of UI switches.

--- plugins/hs/index.ts
+++ plugins/hs/index.ts
@@ -1,41 +1,31 @@
-import { React, ReactNative as RN } from "@vendetta/metro/common";
+import { findByProps } from "@vendetta/metro";
 import { storage } from "@vendetta/plugin";
-import { FormSwitchRow } from "@vendetta/ui/components";
+import { before } from "@vendetta/patcher";
+import Settings from "./settings";
 
-export default () => {
-    const [fakeMute, setFakeMute] = React.useState(storage.fakeMute ?? false);
-    const [fakeDeaf, setFakeDeaf] = React.useState(storage.fakeDeaf ?? false);
-    const [fakeVideo, setFakeVideo] = React.useState(storage.fakeVideo ?? false);
+const GatewayConnectionSocket = findByProps("voiceStateUpdate", "voiceServerPing");
 
-    return (
-        <RN.ScrollView>
-            <FormSwitchRow
-                label="Fake Mute"
-                subLabel="Show others you are muted, but you can still talk (locally unmuted)."
-                value={fakeMute}
-                onValueChange={(value) => {
-                    setFakeMute(value);
-                    storage.fakeMute = value;
-                }}
-            />
-            <FormSwitchRow
-                label="Fake Deafened"
-                subLabel="Show others you are deafened, but you can still hear them."
-                value={fakeDeaf}
-                onValueChange={(value) => {
-                    setFakeDeaf(value);
-                    storage.fakeDeaf = value;
-                }}
-            />
-            <FormSwitchRow
-                label="Fake Video Off"
-                subLabel="Show others your video is off, but you can still stream."
-                value={fakeVideo}
-                onValueChange={(value) => {
-                    setFakeVideo(value);
-                    storage.fakeVideo = value;
-                }}
-            />
-        </RN.ScrollView>
-    );
+let unpatch: () => void;
+
+export default {
+    onLoad: () => {
+        unpatch = before("voiceStateUpdate", GatewayConnectionSocket.prototype, (args) => {
+            const { fakeMute, fakeDeaf, fakeVideo } = storage;
+            const voiceStateUpdateArgs = args[0];
+
+            if (fakeMute) {
+                voiceStateUpdateArgs.selfMute = true;
+            }
+            if (fakeDeaf) {
+                voiceStateUpdateArgs.selfDeaf = true;
+            }
+            if (fakeVideo) {
+                voiceStateUpdateArgs.selfVideo = true;
+            }
+        });
+    },
+    onUnload: () => {
+        unpatch();
+    },
+    settings: Settings,
 };
