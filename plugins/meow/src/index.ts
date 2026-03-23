0793c1b602910f8182bf70ae09922732c593d8f7
6roblox9 <croissantai69@gmail.com>
2026-03-24 01:52:30 +0300

    Update index.ts

--- plugins/meow/src/index.ts
+++ plugins/meow/src/index.ts
@@ -1,83 +1,68 @@
-import { before } from "@vendetta/patcher";
+import { before, after } from "@vendetta/patcher";
 import { getAssetIDByName } from "@vendetta/ui/assets";
-import { clipboard } from "@vendetta/metro/common";
+import { findInReactTree } from "@vendetta/utils";
+import { findByName, findByProps } from "@vendetta/metro";
+import { React, clipboard } from "@vendetta/metro/common";
 import { showToast } from "@vendetta/ui/toasts";
 import { Forms } from "@vendetta/ui/components";
-import { findInReactTree } from "@vendetta/utils";
-
-const { FormRow, FormIcon } = Forms;
 
 const LazyActionSheet = findByProps("openLazy", "hideActionSheet");
+const { FormRow, FormIcon } = Forms;
 
 const unpatch = before("openLazy", LazyActionSheet, ([component, key, msg]) => {
-  const message = msg?.message;
-  if (key !== "MessageLongPressActionSheet" || !message) return;
-  
-  const proxyUrl = message?.attachments?.[0]?.proxy_url;
-  if (!proxyUrl) return;
+    const message = msg?.message;
+    const proxyUrl = message?.attachments?.[0]?.proxy_url;
 
-  component.then((instance) => {
-    const unpatch = after("default", instance, (_, component) => {
-      React.useEffect(
-        () => () => {
-          unpatch();
-        },
-        [],
-      );
+    if (key !== "MessageLongPressActionSheet" || !proxyUrl) return;
 
-      const actionSheetContainer = findInReactTree(
-        component,
-        (x) => Array.isArray(x) && x[0]?.type?.name === "ActionSheetRowGroup",
-      );
-      const buttons = findInReactTree(
-        component,
-        (x) => x?.[0]?.type?.name === "ButtonRow",
-      );
+    component.then((instance) => {
+        const unpatchAfter = after("default", instance, (_, component) => {
+            React.useEffect(() => () => unpatchAfter(), []);
 
-      if (buttons) {
-        buttons.push(
-          React.createElement(FormRow, {
-            label: "Copy Proxy Link",
-            leading: React.createElement(FormIcon, {
-              style: { opacity: 1 },
-              source: getAssetIDByName("ic_link"),
-            }),
-            onPress: () => {
-              clipboard.setString(proxyUrl);
-              showToast("Copied proxy link to clipboard", getAssetIDByName("toast_copy_link"));
-            },
-          })
-        );
-      } else if (actionSheetContainer && actionSheetContainer[1]) {
-        const middleGroup = actionSheetContainer[1];
+            const actionSheetContainer = findInReactTree(
+                component,
+                (x) => Array.isArray(x) && x[0]?.type?.name === "ActionSheetRowGroup",
+            );
+            const buttons = findInReactTree(
+                component,
+                (x) => x?.[0]?.type?.name === "ButtonRow",
+            );
 
-        const ActionSheetRow = middleGroup.props.children[0].type;
+            const copyAction = () => {
+                LazyActionSheet.hideActionSheet();
+                clipboard.setString(proxyUrl);
+                showToast("Copied Proxy Link", getAssetIDByName("toast_copy_link"));
+            };
 
-        const copyProxyLinkButton = React.createElement(ActionSheetRow, {
-          label: "Copy Proxy Link",
-          icon: {
-            $$typeof: middleGroup.props.children[0].props.icon.$$typeof,
-            type: middleGroup.props.children[0].props.icon.type,
-            key: null,
-            ref: null,
-            props: {
-              IconComponent: () => React.createElement(FormIcon, {
-                style: { opacity: 1 },
-                source: getAssetIDByName("ic_link"),
-              }),
-            },
-          },
-          onPress: () => {
-            clipboard.setString(proxyUrl);
-            showToast("Copied proxy link to clipboard", getAssetIDByName("toast_copy_link"));
-          },
-          key: "copy-proxy-link",
+            if (buttons) {
+                buttons.push(
+                    <FormRow
+                        label="Copy Proxy Link"
+                        leading={<FormIcon style={{ opacity: 1 }} source={getAssetIDByName("ic_link")} />}
+                        onPress={copyAction}
+                    />,
+                );
+            } else if (actionSheetContainer && actionSheetContainer[1]) {
+                const middleGroup = actionSheetContainer[1];
+                const ActionSheetRow = middleGroup.props.children[0].type;
+
+                middleGroup.props.children.push(
+                    <ActionSheetRow
+                        label="Copy Proxy Link"
+                        icon={{
+                            $$typeof: middleGroup.props.children[0].props.icon.$$typeof,
+                            type: middleGroup.props.children[0].props.icon.type,
+                            props: {
+                                IconComponent: () => <FormIcon style={{ opacity: 1 }} source={getAssetIDByName("ic_link")} />,
+                            },
+                        }}
+                        onPress={copyAction}
+                        key="copy-proxy-link"
+                    />,
+                );
+            }
         });
-
-        middleGroup.props.children.push(copyProxyLinkButton);
-      }
     });
-  });
 });
 
 export const onUnload = () => unpatch();
